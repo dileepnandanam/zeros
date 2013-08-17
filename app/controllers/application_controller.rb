4b954fcbbe5611252545f67cd8ace123604111c9
dileepnandanam <dileepsnandanam@gmail.com>
2013-08-18 01:04:54 +0530

    added return path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  before_filter { |c| cookies.permanent[:return_to] = request.path if request.get? && !request.xhr? && request.path != '/auth/facebook/callback' }
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
       user = User.find_or_create_by_uid(env["omniauth.auth"][:uid])
       user.update_attributes(access_token: env["omniauth.auth"][:credentials][:token], login: env["omniauth.auth"][:info][:name], avathar: env["omniauth.auth"][:info][:image])
       sign_in(user)
-      redirect_to games_dashboard_path
+      redirect_back
     else
       user = User.find_by_login(params[:session][:login])
       if user && user.authenticate?(params[:session][:password])
         sign_in(user)
-        redirect_to games_dashboard_path
+        redirect_back
       else
         render 'new'
         flash[:notice] = 'wrong'
@@ -24,5 +24,13 @@ class SessionsController < ApplicationController
     @current_user = nil
     cookies.permanent[:remember_token] = nil
     redirect_to games_path
+  end
+
+  def redirect_back
+    if cookies[:return_to].present?
+      redirect_to cookies[:return_to]
+    else
+      redirect_to games_dashboard_path
+    end
   end
 end
