1c9aa9c393fed15400c138e3e434fe96c07edf6b
dileepnandanam <dileepsnandanam@gmail.com>
2013-08-18 23:26:54 +0530

    pgsql

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,7 +14,7 @@ class GamesController < ApplicationController
 
   def index
     @sample_games = Game.find :all, limit: 5, conditions:{ co_player_id: nil }
-    @finished_games = Game.find :all, limit: 5, conditions:"winner_id not null"
+    @finished_games = Game.find :all, limit: 5, conditions:"winner_id is not null",order: "created_at DESC"
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @sample_games }
